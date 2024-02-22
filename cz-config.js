c7b12d99a8449f5129bb0b3634d8e0579d77f7ab
--local <--local>
2024-02-22 12:59:36 -0600

    ✨ feature(custom): add logs

---
+++ cz-config.js
@@ -0,0 +1,8 @@
+// .cz-config.js
+module.exports = {
+  types: [
+    { value: '✨ feature', name: '✨ feature:     A new feature' },
+    { value: '🐞 fix', name: '🐞 fix:      A bug fix' },
+    { value: '🚀 hotfix', name: '🚀 hotfix:   A critical hotfix' },
+  ],
+};
